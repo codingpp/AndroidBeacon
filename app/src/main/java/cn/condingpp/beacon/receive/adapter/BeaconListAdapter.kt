9a9d5be991ea41e5c776035eaa0865530a393e4e
sunpan <sunpan920@gmail.com>
2021-01-23 20:20:10 +0800

    major和minor输入限制

--- app/src/main/java/cn/condingpp/beacon/receive/adapter/BeaconListAdapter.kt
+++ app/src/main/java/cn/condingpp/beacon/receive/adapter/BeaconListAdapter.kt
@@ -1,7 +1,6 @@
 package cn.condingpp.beacon.receive.adapter
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
