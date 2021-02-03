dbb8384ac6e5faf1aa0c1a41ad9ba8d4e9366a2e
sunpan <sunpan920@gmail.com>
2021-02-03 14:57:40 +0800

    权限请求框架替换为permissionx

--- app/src/main/java/cn/condingpp/beacon/MainActivity.java
+++ app/src/main/java/cn/condingpp/beacon/MainActivity.java
@@ -3,15 +3,21 @@ package cn.condingpp.beacon;
 import android.Manifest;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.permissionx.guolindev.PermissionX;
+import com.permissionx.guolindev.callback.ExplainReasonCallback;
+import com.permissionx.guolindev.callback.RequestCallback;
+import com.permissionx.guolindev.request.ExplainScope;
+
+import java.util.List;
+
 import cn.codingpp.beacon.databinding.ActivityMainBinding;
 import cn.condingpp.beacon.broadcast.BroadcastActivity;
 import cn.condingpp.beacon.receive.ReceiveActivity;
-import cn.condingpp.beacon.receive.permission.RequestCallback;
-import cn.condingpp.beacon.receive.permission.RxPermissionRequest;
 
 /**
  * 主页
@@ -50,19 +56,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * 请求权限
+     */
     private void requestPermission() {
-        RxPermissionRequest request = new RxPermissionRequest();
-        request.request(this, new RequestCallback() {
+        PermissionX.init(this)
+                .permissions(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+                .onExplainRequestReason(new ExplainReasonCallback() {
+                    @Override
+                    public void onExplainReason(ExplainScope scope, List<String> deniedList) {
+                        scope.showRequestReasonDialog(deniedList, "即将重新申请的权限是程序必须依赖的权限", "好的", "取消");
+                    }
+                }).request(new RequestCallback() {
             @Override
-            public void onRequestPermissionSuccess() {
-
+            public void onResult(boolean allGranted, List<String> grantedList, List<String> deniedList) {
+                if (!allGranted) {
+                    Toast.makeText(MainActivity.this, "权限已拒绝", Toast.LENGTH_SHORT).show();
+                }
             }
-
-            @Override
-            public void onRequestPermissionFailure() {
-
-            }
-        }, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION);
+        });
     }
 
 }
